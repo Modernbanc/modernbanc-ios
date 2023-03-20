f6204ca142b400d98fc8017cb3c55fb05bb6c588
Gregory Gevorkyan <gregory@modernbanc.com>
2023-03-20 14:22:39 +0000

    Init tests

--- Tests/modernbanc-iosTests/modernbanc_iosTests.swift
+++ Tests/modernbanc-iosTests/modernbanc_iosTests.swift
@@ -6,6 +6,7 @@ final class modernbanc_iosTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(modernbanc_ios().text, "Hello, World!")
+        let client = ModernbancApiClient(api_key: "test_key")
+        XCTAssertNotNil(client)
     }
 }
